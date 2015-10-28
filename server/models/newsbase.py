f8202212d62f9026c1f3e118791035d9a61677f0
AosenZhang <316052486@qq.com>
2015-10-28 11:27:29 +0800

    修复排序bug

--- server/models/newsbase.py
+++ server/models/newsbase.py
@@ -9,12 +9,12 @@ class NewsBase(Base):
 
     def loadSinaGirlPic(self, cl, page, limit):
         if cl:
-            sql = """SELECT * FROM girlpic WHERE cl=%s LIMIT %s, %s"""
+            sql = """SELECT * FROM girlpic WHERE cl=%s ORDER BY -createtime LIMIT %s, %s"""
             return self.db.query(sql, cl, (page-1)*limit, limit)
         else:
-            sql = """SELECT * FROM girlpic LIMIT %s, %s"""
+            sql = """SELECT * FROM girlpic ORDER BY -createtime LIMIT %s, %s"""
             return self.db.query(sql, (page-1)*limit, limit)
 
     def loadNews(self, tag, page, limit):
-        sql = "SELECT * FROM news WHERE tag=%s LIMIT %s, %s"
+        sql = "SELECT * FROM news WHERE tag=%s ORDER BY -createtime LIMIT %s, %s"
         return self.db.query(sql, tag, (page-1)*limit, limit)
